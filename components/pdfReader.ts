b75709bad79ee3c4a73f2eeac2f65e2776adb2a3
Gerardo <gcarrillo898989@gmail.com>
2026-04-27 19:17:42 -0600

    Fix: lector de PDF en cliente y solución a DOMMatrix (pdfjs-dist)

--- components/pdfReader.ts
+++ components/pdfReader.ts
@@ -1,6 +1,4 @@
-"use client";
-
-import { extractTextFromPDF } from "@/components/pdfReader";
+import * as pdfjsLib from "pdfjs-dist";
 
 pdfjsLib.GlobalWorkerOptions.workerSrc = `//cdnjs.cloudflare.com/ajax/libs/pdf.js/${pdfjsLib.version}/pdf.worker.min.js`;
 
